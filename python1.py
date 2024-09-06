aa7671591f1913520f23e355258e4a95c0835808
Adilijh <akstyle201471@gmail.com>
2024-09-06 16:55:55 +0530

    Added another sciprt to make decing whether i want to study or play game

--- python1.py
+++ python1.py
@@ -4,3 +4,10 @@ if weather == "yes":
     print("You should take an umbrella.")
 else:
     print("No need to take an umbrella.")
+
+mood = input("Are you feeling productive? (yes/no): ").strip().lower()
+
+if mood == "yes":
+    print("You should study!")
+else:
+    print("You can play a game, but don't forget to study later!")
